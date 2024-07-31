106a6258b274cc20d151a48c3aa06da628435c18
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-07-31 18:46:35 +0200

    c7

--- test_aha-viedeo.py
+++ test_aha-viedeo.py
@@ -4,4 +4,4 @@ st.header("Schau dir dieses Viedeo an!")
 st.subheader("Watch this viedeo!")
 
 url_viedeo='https://github.com/LaraFriedrichs/test_aha_viedeo/blob/main/data/viedeo_test.mp4'
-st.video(url_viedeo)
+st.video(url_viedeo,format="video/mp4",)
