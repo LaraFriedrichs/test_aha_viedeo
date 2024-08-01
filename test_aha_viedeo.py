d189adcc437a54553db7608631244c0ce598d5cc
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-08-01 09:34:00 +0200

    c12

--- test_aha_viedeo.py
+++ test_aha_viedeo.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
-st.header("Schau dir dieses Viedeo an!")
-st.subheader("Watch this viedeo!")
+st.header("Schau dir dieses Video an!")
+st.subheader("Watch this video!")
 
-#viedeo_file=open('C:/Users/49176/OneDrive/Desktop/test_aha_viedeo/viedeo_test.mp4')
+#video_file=open('C:/Users/49176/OneDrive/Desktop/test_aha_viedeo/viedeo_test.mp4')
 #viedeo_bytes=viedeo_file.read()
 
 #st.viedeo(viedeo_bytes)
 
 #url_viedeo='https://github.com/LaraFriedrichs/test_aha_viedeo/blob/main/data/viedeo_test.mp4'
-#url_viedeo='https://raw.githubusercontent.com/LaraFriedrichs/test_aha_viedeo/main/data/viedeo_test.mp4'
-url_video="https://github.com/Larafriedrichs/test_aha_viedeo/raw/main/viedeo_test.mp4"
-#st.video(url_viedeo)
+url_video='https://raw.githubusercontent.com/LaraFriedrichs/test_aha_viedeo/main/data/viedeo_test.mp4'
+#url_video="https://github.com/Larafriedrichs/test_aha_viedeo/raw/main/viedeo_test.mp4"
+st.video(url_video)
 
-video_file=open(url_video)
-video_bytes=video_file.read()
+#video_file=open(url_video)
+#video_bytes=video_file.read()
 
-st.video(viedeo_bytes)
+#st.video(video_bytes)
