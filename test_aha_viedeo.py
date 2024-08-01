3afa9f1985e428c32d01abb5eff7d01e559bf76a
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-08-01 10:20:14 +0200

    c14

--- test_aha_viedeo.py
+++ test_aha_viedeo.py
@@ -1,17 +1,27 @@
+# import streamlit as st
+
+# st.header("Schau dir dieses Video an!")
+# st.subheader("Watch this video!")
+
+
+# #url_viedeo='https://github.com/LaraFriedrichs/test_aha_viedeo/blob/main/data/viedeo_test.mp4'
+# url_video='https://raw.githubusercontent.com/LaraFriedrichs/test_aha_viedeo/main/data/viedeo_test.mp4'
+# #url_video="https://github.com/Larafriedrichs/test_aha_viedeo/raw/main/viedeo_test.mp4"
+# st.video(url_video)
+
+# #video_file=open(url_video)
+# #video_bytes=video_file.read()
+
+# #st.video(video_bytes)
+
+# st.markdown("info")
+
 import streamlit as st
 
 st.header("Schau dir dieses Video an!")
 st.subheader("Watch this video!")
 
-
-#url_viedeo='https://github.com/LaraFriedrichs/test_aha_viedeo/blob/main/data/viedeo_test.mp4'
-url_video='https://raw.githubusercontent.com/LaraFriedrichs/test_aha_viedeo/main/data/viedeo_test.mp4'
-#url_video="https://github.com/Larafriedrichs/test_aha_viedeo/raw/main/viedeo_test.mp4"
+url_video = 'https://raw.githubusercontent.com/LaraFriedrichs/test_aha_viedeo/main/data/viedeo_test.mp4'
 st.video(url_video)
-
-#video_file=open(url_video)
-#video_bytes=video_file.read()
-
-#st.video(video_bytes)
 
 st.markdown("info")
