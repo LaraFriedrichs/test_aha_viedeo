2b49fed26490a8ff96016ea60908b9037e9cdddf
LaraFriedrichs <152259928+LaraFriedrichs@users.noreply.github.com>
2024-08-01 10:24:39 +0200

    c15

--- test_aha_viedeo.py
+++ test_aha_viedeo.py
@@ -21,7 +21,8 @@ import streamlit as st
 st.header("Schau dir dieses Video an!")
 st.subheader("Watch this video!")
 
-url_video = 'https://raw.githubusercontent.com/LaraFriedrichs/test_aha_viedeo/main/data/viedeo_test.mp4'
+#url_video = 'https://raw.githubusercontent.com/LaraFriedrichs/test_aha_viedeo/main/data/viedeo_test.mp4'
+url_video='https://github.com/LaraFriedrichs/test_aha_viedeo/raw/main/viedeo_test.mp4'
 st.video(url_video)
 
 st.markdown("info")
